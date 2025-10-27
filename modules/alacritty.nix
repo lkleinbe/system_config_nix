74eccddab9d3c49405f9685ec9ddb7024602c537
System administrator <leonard@kleinberger.de>
2025-10-27 11:09:13 +0100

    Light mode improvements and openair reqirements

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -21,30 +21,30 @@
           size = 10
 
         [colors.primary]
-          background = '#1e2127'
-          foreground = '#abb2bf'
-          bright_foreground = '#e6efff'
+          background = '0x1f2329'
+          foreground = '0xa0a8b7'
+          dim_foreground = '0xa0a8b7'
+          bright_foreground = '0xa0a8b7'
 
         [colors.normal]
-          black=   '#1e2127'
-          red=     '#e06c75'
-          green=   '#98c379'
-          yellow=  '#d19a66'
-          blue=    '#61afef'
-          magenta= '#c678dd'
-          cyan=    '#56b6c2'
-          white=   '#828791'
-
+          black = '0x0e1013'
+          red = '0xe55561'
+          green = '0x8ebd6b'
+          yellow = '0xe2b86b'
+          blue = '0x4fa6ed'
+          magenta = '0xbf68d9'
+          cyan = '0x48b0bd'
+          white = '0x7a818e'
 
         [colors.bright]
-          black=   '#5c6370'
-          red=     '#e06c75'
-          green=   '#98c379'
-          yellow=  '#d19a66'
-          blue=    '#61afef'
-          magenta= '#c678dd'
-          cyan=    '#56b6c2'
-          white=   '#e6efff'
+          black = '0x535965' 
+          red = '0xe55561'
+          green = '0x8ebd6b'
+          yellow = '0xe2b86b'
+          blue = '0x4fa6ed'
+          magenta = '0xbf68d9'
+          cyan = '0x48b0bd'
+          white = '0xa0a8b7'
 
         [colors.dim]
           black=   '#1e2127'
